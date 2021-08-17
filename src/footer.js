e5917c1344f24b2b3877ba0dd477895bcb76ef8a
Abhishekmohan7171 <abhishekmohan7171@gmail.com>
2021-08-17 13:03:16 +0530

    1st Assignment

---
+++ src/footer.js
@@ -0,0 +1,8 @@
+function Footer(){
+    let name = "Abhishek";
+    return(
+        <h2>Bye bye {name}</h2>
+    );
+}
+
+export default Footer;
---
+++ src/header.js
@@ -0,0 +1,10 @@
+
+
+function Header(){
+
+    return(
+        <h1>Iam Header</h1>
+    );
+}
+
+export default Header;
